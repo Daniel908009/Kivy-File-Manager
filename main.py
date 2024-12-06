974a2c9ea30773ed720b9063cb282d04217873f0
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-06 20:45:28 +0100

    added the file selection popup, did the overall UI design

--- main.py
+++ main.py
@@ -1,50 +1,50 @@
-from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
-from kivy.uix.button import Button
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
-from kivy.uix.textinput import TextInput
 from kivy.uix.popup import Popup
-from kivy.uix.gridlayout import GridLayout
-from kivy.uix.scrollview import ScrollView
-from kivy.uix.filechooser import FileChooserListView
 
 
-
-
-
-
-
-
+class File_selecting_popup(Popup):
+    def send_info(self, caller):
+        self.calller = caller
+    def cancel(self):
+        self.dismiss()
+    def select(self):
+        print("Selected")
+        self.dismiss()
 
 class Key_Word_SearchScreen(Screen):
     def main_menu_screen(self):
-        self.manager.transition = SlideTransition(direction='right')
+        self.manager.transition = SlideTransition(direction='left')
         self.manager.transition.duration = 1.5
         self.manager.current = 'main'
     def sort_files_screen(self):
-        self.manager.transition = SlideTransition(direction='left')
+        self.manager.transition = SlideTransition(direction='right')
         self.manager.transition.duration = 1.5
         self.manager.current = 'file_sort'
+    def select_directories(self):
+        popup = File_selecting_popup()
+        popup.open()
 
 class File_SortScreen(Screen):
     def main_menu_screen(self):
-        self.manager.transition = SlideTransition(direction='left')
+        self.manager.transition = SlideTransition(direction='right')
         self.manager.transition.duration = 1.5
         self.manager.current = 'main'
     def find_keywords_screen(self):
-        self.manager.transition = SlideTransition(direction='right')
-        self.manager.transition.duration = 1.5
-        self.manager.current = 'key_word_search'
-
-
-class MainScreen(Screen):
-    def find_keywords_screen(self):
         self.manager.transition = SlideTransition(direction='left')
         self.manager.transition.duration = 1.5
         self.manager.current = 'key_word_search'
-    def sort_files_screen(self):
+    def select_directories(self):
+        popup = File_selecting_popup()
+        popup.open()
+
+class MainScreen(Screen):
+    def find_keywords_screen(self):
         self.manager.transition = SlideTransition(direction='right')
+        self.manager.transition.duration = 1.5
+        self.manager.current = 'key_word_search'
+    def sort_files_screen(self):
+        self.manager.transition = SlideTransition(direction='left')
         self.manager.transition.duration = 1.5
         self.manager.current = 'file_sort'
 
