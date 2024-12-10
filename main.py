204262f984893bf4763f1343deac5cb24e5a6475
Daniel Kriz <147802055+Daniel908009@users.noreply.github.com>
2024-12-10 18:50:27 +0100

    First version with both of the windows working correctly, it is still far from completed though.
    also added requirements.txt and readme file

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from kivy.uix.actionbar import Label
 from kivy.uix.gridlayout import GridLayout
 from kivy.app import App
 from kivy.uix.screenmanager import ScreenManager, Screen, SlideTransition
@@ -9,9 +10,11 @@ current_directory = os.getcwd()
 # popup that is used for selecting files in the key word search screen, maybe will be enhanced to cover both screens in the future
 class File_selecting_popup(Popup):
     
-    def send_info(self, caller):
+    def send_info(self, caller, filter_files):
         self.caller = caller
         self.ids.filechooser.path = current_directory
+        if filter_files != None:
+            self.ids.filechooser.filters = [filter_files]
 
     def cancel(self):
         self.dismiss()
@@ -71,7 +74,7 @@ class Key_Word_SearchScreen(Screen):
 
     def select_directories(self):
         popup = File_selecting_popup()
-        popup.send_info(self)
+        popup.send_info(self, ['*.txt', '*.json']) # will most likely be changed, I just need to test it some things first
         popup.open()
 
     def search(self):
@@ -131,10 +134,107 @@ class File_SortScreen(Screen):
 
     def select_directories(self):
         popup = File_selecting_popup()
+        popup.send_info(self, None)
         popup.open()
+
+    def sort(self):
+        if len(self.all_selected_files) > 0 and self.ids.sort_method.text != 'Select sorting method' and self.ids.original_files.text != 'Select action':
+            if self.ids.sort_method.text == 'Sort by types':
+                self.sort_by_types()
+            elif self.ids.sort_method.text == 'Sort by size':
+                self.sort_by_size_pop()
+            elif self.ids.sort_method.text == 'Sort by date':
+                self.sort_by_date()
+    
+    def sort_by_types(self):
+        # getting all the types of the selected files
+        types = []
+        for file in self.all_selected_files:
+            name = file.split("/")[-1]
+            if "." in name:
+                type = name.split(".")[-1]
+                if type not in types:
+                    types.append(type)
+        # creating directories for each type
+        for type in types:
+            os.mkdir(current_directory + "/" + type + "_files")
+        # moving the files to the directories or copying them, depends on the selected action
+        if self.ids.original_files.text == 'Use original files':
+            for file in self.all_selected_files:
+                name = file.split("/")[-1]
+                type = name.split(".")[-1]
+                os.rename(file, current_directory + "/" + type + "_files/" + name)
+        elif self.ids.original_files.text == 'Use copies':
+            for file in self.all_selected_files:
+                name = file.split("/")[-1]
+                type = name.split(".")[-1]
+                os.system("cp " + file + " " + current_directory + "/" + type + "_files/" + name)
+    
+    def sort_by_size_pop(self):
+        # getting the size bariers
+        self.size_bariers = []
+        sizeBariersPop = SizeBariersPopup()
+        sizeBariersPop.send_info(self)
+        sizeBariersPop.open()
+
+    def sort_by_date(self):
+        pass # will be implemented later
+    
+    def sort_by_size(self):
+        # adding zero to the size bariers in case it isnt there
+        if 0 not in self.size_bariers:
+            self.size_bariers.append(0)
+        # reordering the size bariers
+        self.size_bariers.sort()
+        # creating directories for between the size bariers
+        for i in range(len(self.size_bariers) - 1):
+            os.mkdir(current_directory + "/" + str(self.size_bariers[i]) + "-" + str(self.size_bariers[i + 1]) + "_files")
+        # adding the final directory for everything above the last size barier
+        os.mkdir(current_directory + "/" + str(self.size_bariers[-1]) + "and_more_files")
+        # moving the files to the directories or copying them, depends on the selected action
+        if self.ids.original_files.text == 'Use original files':
+            for file in self.all_selected_files:
+                name = file.split('/')[-1]
+                size = os.path.getsize(file)
+                for i in range(len(self.size_bariers) - 1):
+                    if size >= self.size_bariers[i] and size < self.size_bariers[i + 1]:
+                        os.rename(file, current_directory + "/" + str(self.size_bariers[i]) + "-" + str(self.size_bariers[i + 1]) + "_files/" + name)
+                if size >= self.size_bariers[-1]:
+                    os.rename(file, current_directory + "/" + str(self.size_bariers[-1]) + "and_more_files/" + name)
+        elif self.ids.original_files.text == 'Use copies':
+            for file in self.all_selected_files:
+                name = file.split('/')[-1]
+                size = os.path.getsize(file)
+                for i in range(len(self.size_bariers) - 1):
+                    if size >= self.size_bariers[i] and size < self.size_bariers[i + 1]:
+                        os.system("cp " + file + " " + current_directory + "/" + str(self.size_bariers[i]) + "-" + str(self.size_bariers[i + 1]) + "_files/" + name)
+                if size >= self.size_bariers[-1]:
+                    os.system("cp " + file + " " + current_directory + "/" + str(self.size_bariers[-1]) + "and_more_files/" + name)
+
+# popup used for selecting size bariers
+class SizeBariersPopup(Popup):
+    def send_info(self, caller):
+        self.caller = caller
+    def cancel(self):
+        self.dismiss()
+    def add(self):
+        self.ids.selected_files_title.text = "Selected Size Bariers"
+        if self.ids.size_barier.text != "" and self.ids.size_barier.text not in self.selected:
+            self.selected.append(self.ids.size_barier.text)
+            self.ids.selected.add_widget(Label(text=self.ids.size_barier.text, height=70, font_size=20)) # later will be widget
+            self.ids.selected.height += 70
+            self.ids.size_barier.text = ""
+    def select(self):
+        for barier in self.selected:
+            self.caller.size_bariers.append(int(barier))
+        self.caller.sort_by_size()
+        self.dismiss()
 
 # main/opening screen
 class MainScreen(Screen):
+
+    def get_joke(self): # this is will be done later
+        pass
 
     def find_keywords_screen(self):
         self.manager.transition = SlideTransition(direction='right')
@@ -150,10 +250,12 @@ class MainScreen(Screen):
 class File_ManagerApp(App):
 
     def build(self):
+        # this block of code is the switch screen manager setup
         sm = ScreenManager()
         sm.add_widget(MainScreen(name='main'))
         sm.add_widget(Key_Word_SearchScreen(name='key_word_search'))
         sm.add_widget(File_SortScreen(name='file_sort'))
+
         return sm
     
 # running the app
